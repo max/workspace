5d9fddffdddbc43ed3a4b1f0a64f783c6d2fddbc
Max Schoening <max@max.wtf>
2019-04-16 01:29:08 +0000

    Add act

--- workstation/entrypoint.sh
+++ workstation/entrypoint.sh
@@ -2,13 +2,13 @@
 
 set -e
 
-if [ ! -d ~/code/workspace ]; then
+if [ ! -d ~/code/max/workspace ]; then
   echo "Cloning dotfiles"
   cd ~/code
-  git clone git@github.com:max/workspace.git
+  git clone git@github.com:max/workspace.git max/workspace
 fi
 
-cd ~/code/workspace
+cd ~/code/max/workspace
 
 ln -s $(pwd)/vimrc ~/.vimrc
 ln -s $(pwd)/zshrc ~/.zshrc
