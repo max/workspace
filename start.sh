2cc2b0c94bf9e4f55f67a677f063296f1b855b01
Max Schoening <max@max.wtf>
2019-04-19 22:07:19 +0000

    Mount local .config directory to allow for hub auth

--- start.sh
+++ start.sh
@@ -5,8 +5,9 @@ docker run \
   -h dev \
   --rm \
   -v /var/run/docker.sock:/var/run/docker.sock \
-  -v $HOME/Code:/root/code \
   -v $HOME/.ssh:/root/.ssh \
+  -v $HOME/.config:/root/.config \
+  -v $HOME/Code:/root/code \
   --privileged \
   --name dev \
   max/workstation
